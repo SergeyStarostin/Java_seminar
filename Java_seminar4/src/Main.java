582eef247aa7e267f9292a832917efd65730b9ce
Starostin Sergey <Vlillen@yandex.ru>
2023-06-20 20:52:45 +0400

    added task conditions

---
+++ Java_seminar4/src/Main.java
@@ -0,0 +1,10 @@
+import Collection.LinkListSolution;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+//        ListTest.TimeTest(1_000_00);
+        LinkListSolution.run();
+
+    }
+}
