777d61cf27a5c30f5a87baa0c91beea35349a79c
Starostin Sergey <Vlillen@yandex.ru>
2023-06-20 22:40:04 +0400

    added task3

--- Java_seminar5/src/App.java
+++ Java_seminar5/src/App.java
@@ -1,9 +1,27 @@
 import java.util.HashMap;
 
-public class App {
-
+public class App {   
     public static void main(String[] args) throws Exception{
-        HashMap<Integer,String> map =  HashMapTest.Test.fill();
-        HashMapTest.Test.printHashMap(map,"Иванов");
+        //HashMap<Integer,String> map =  HashMapTest.Test.fill();
+        //HashMapTest.Test.printHashMap(map,"Иванов");
+
+        HashMap<String, Integer> open = new HashMap<>();
+        open.put("(", 0);
+        open.put("[", 1);
+        open.put("{", 2);
+        open.put("<", 3);
+
+        HashMap<String, Integer> close = new HashMap<>();
+        close.put(")", 0);
+        close.put("]", 1);
+        close.put("}", 2);
+        close.put(">", 3);
+
+        System.out.println(BracketTest.Tester.validation(open, close, "a+(d*3)"));
+        System.out.println(BracketTest.Tester.validation(open, close, "[a+(1*3)"));
+        System.out.println(BracketTest.Tester.validation(open, close, "[6+(3*3)] "));
+        System.out.println(BracketTest.Tester.validation(open, close, "{a}[+]{(d*3)} "));
+        System.out.println(BracketTest.Tester.validation(open, close, "<{a}+{(d*3)}>"));
+        System.out.println(BracketTest.Tester.validation(open, close, "{a+]}{(d*3)} "));
     }
 }
