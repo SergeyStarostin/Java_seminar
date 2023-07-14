257b721955c0fe8e2bbada295044a4c8f9bf0a18
Y127908 <Sergey.starostin@vaz.ru>
2023-07-14 16:54:41 +0400

    Deleted comment

--- OOP_Seminar_2/src/App.java
+++ OOP_Seminar_2/src/App.java
@@ -8,7 +8,6 @@ import Interfaces.iActorBehaviuor;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
         Market magnit = new Market();
         iActorBehaviuor client1 = new OrdinaryClient("Boris");
         iActorBehaviuor client2 = new SpecialClient("prezident", 1001);
