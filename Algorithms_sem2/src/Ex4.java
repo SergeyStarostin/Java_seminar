13b09ff274d4edcea028b01e18a87eca994490a2
Starostin Sergey <Vlillen@yandex.ru>
2023-08-10 23:18:56 +0400

    added tasks for seminar 2

--- Algorithms_sem2/src/Ex4.java
+++ Algorithms_sem2/src/Ex4.java
@@ -10,6 +10,14 @@ public class Ex4 {
         Ex1.printArr(array);
         System.out.println(search(array, 5, 0, array.length - 1));
     }
+
+     /**
+     * @apiNote Метод бинарного поиска элемента
+     * @param arrray - исходный массив
+     * @param value - значение элемента
+     * @param min - минимальное значение элемента
+     * @param max - максимальное значение элемента
+     */
     public static int search(int [] array, int value, int min, int max) {
         int middle;
         if (max < min) {
