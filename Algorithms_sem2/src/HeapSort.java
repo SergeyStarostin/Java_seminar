d1d4f701a9c676404ea4b439b9b44bd6e32d9a86
Y127908 <Sergey.starostin@vaz.ru>
2023-08-15 14:27:24 +0400

    Added home work for 3 seminar

--- Algorithms_sem2/src/HeapSort.java
+++ Algorithms_sem2/src/HeapSort.java
@@ -70,7 +70,6 @@ public class HeapSort {
      */
     public static void main(String args[]) {
         int arr[] = { 12, 11, 13, 5, 6, 7 };
-        // int n = arr.length;
 
         System.out.println("Исходный массив: ");
         printArray(arr);
